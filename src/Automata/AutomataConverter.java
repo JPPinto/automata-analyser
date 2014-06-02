5dd847f66f1bcd22d10323cc6630fe6d6981c62d
Eduardo Fernandes <e.luis@icloud.com>
2014-06-02 22:07:09 +0100

    Merge linux/windows workspace

--- src/Automata/AutomataConverter.java
+++ src/Automata/AutomataConverter.java
@@ -1,4 +1,12 @@
 package Automata;
 
 public class AutomataConverter {
+
+    static Automata convertNFAtoDFA(Automata originalAutomaton){
+        Automata copy = originalAutomaton.getCopy();
+
+        Automata result = new Automata();
+
+        return result;
+    }
 }
