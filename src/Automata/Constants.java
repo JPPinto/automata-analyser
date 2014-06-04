985644efd9dff03c24abd35865f2fa8bbb831d33
JDSBCC <joo_correia@hotmail.com>
2014-06-04 12:18:52 +0100

    coisas2

--- src/Automata/Constants.java
+++ src/Automata/Constants.java
@@ -10,7 +10,10 @@
 
 package Automata;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 
 public class Constants {
     /* Gui constants */
@@ -19,7 +22,10 @@ public class Constants {
     public static final Color guiDefaultStateColor = Color.ORANGE;
     public static final Color guiDefaultStateBorderColor = new Color(247,150,70);
     public static final Color guiAccepetanceStateBorderColor = new Color(75, 172, 198);
-    public static final Dimension guiDefaultWindowSize = new Dimension(420, 316);
+    
+    public static int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+    public static int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+    public static final Dimension guiDefaultWindowSize = new Dimension(430*1366/width, 330*768/height);
 
     /* File constants */
     public static final int maxFileSize = 4096;
