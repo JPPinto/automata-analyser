2802f4143ae1c3504a845a892c62a19de08f5682
Eduardo Fernandes <e.luis@icloud.com>
2014-06-06 18:05:01 +0100

    Revert "change size"
    This reverts commit b33711e340ead02ff2c2134d772d0cd3302047a2.

--- src/Automata/Constants.java
+++ src/Automata/Constants.java
@@ -21,11 +21,11 @@ public class Constants {
     public static final Color guiDefaultBackgroundColor = Color.decode("#FAFBFF");
     public static final Color guiDefaultStateColor = Color.ORANGE;
     public static final Color guiDefaultStateBorderColor = new Color(247,150,70);
-    public static final Color guiAcceptanceStateBorderColor = new Color(75, 172, 198);
+    public static final Color guiAccepetanceStateBorderColor = new Color(75, 172, 198);
     
     public static int width = Toolkit.getDefaultToolkit().getScreenSize().width;
     public static int height = Toolkit.getDefaultToolkit().getScreenSize().height;
-    public static final Dimension guiDefaultWindowSize = new Dimension(width/3, height/2);
+    public static final Dimension guiDefaultWindowSize = new Dimension(430*1366/width, 318*768/height);
 
     /* File constants */
     public static final int maxFileSize = 4096;
